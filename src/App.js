8babe766d214c1a19c8537b37ab82b4eeb55b623
janakbharadwaj <janakbharadwaj.adluri@gmail.com>
2021-02-02 17:09:00 +0530

    discussions - static

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./App.module.css";
 import Deals from "./Components/Deals/Deals";
+import { Discussions } from "./Components/Discussions/Discussions";
 import Mentors from "./Components/Mentors/Mentors"
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <h1>App</h1>
       <Deals />
       <Mentors/>
+      <Discussions/>
     </div>
   );
 };
---
+++ src/Components/Discussions/Discussions.jsx
@@ -0,0 +1,201 @@
+import React from 'react'
+//import { DiscussionsRoutes } from './Routes/Routes'
+import styles from './discussions.module.css'
+
+let data =[
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"02",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/3168868/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"32",
+        title:"What are you doing to make your business survive or thrive during the pandemic?",
+        caption:"by Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"04",
+        title:"Audio-first community managers",
+        caption:"by Ankit Dhawan • 10 comments • 1d ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"06",
+        title:"Where do you find interesting live streaming to watch?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"02",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"12",
+        title:"Audio-first community managers",
+        caption:"by Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"23",
+        title:"What are you doing to make your business survive or thrive during the pandemic?",
+        caption:"by Ankit Dhawan • 10 comments • 1d ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"12",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"02",
+        title:"Where do you find interesting live streaming to watch?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/3168868/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"32",
+        title:"Audio-first community managers",
+        caption:"by Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"04",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Ankit Dhawan • 10 comments • 1d ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"06",
+        title:"What are you doing to make your business survive or thrive during the pandemic?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"02",
+        title:"Where do you find interesting live streaming to watch?",
+        caption:"by Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"12",
+        title:"Audio-first community managers",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"23",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Ankit Dhawan • 10 comments • 1d ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"12",
+        title:"Where do you find interesting live streaming to watch?",
+        caption:"bby Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"02",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/3168868/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"32",
+        title:"What are you doing to make your business survive or thrive during the pandemic?",
+        caption:"by Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"04",
+        title:"Audio-first community managers",
+        caption:"by Ankit Dhawan • 10 comments • 1d ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"06",
+        title:"Where do you find interesting live streaming to watch?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"02",
+        title:"Can you convince me that without Excel you won't have data?",
+        caption:"by Lior Barak • 0 comments • 13h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2239099/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"12",
+        title:"Audio-first community managers",
+        caption:"by Mayank Singh Chauhan • 0 comments • 24h ago"
+    },
+    {
+        img:"https://ph-avatars.imgix.net/2789663/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=20&h=20&fit=crop",
+        num:"23",
+        title:"What are you doing to make your business survive or thrive during the pandemic?",
+        caption:"by Ankit Dhawan • 10 comments • 1d ago"
+    }
+]
+
+const Discussions = () => {
+    return (
+        <div className={styles.discussionsPage}>
+            <div>
+                <h1>discussions page</h1>
+                {/* <DiscussionsRoutes/> */}
+            </div>
+            <div className={styles.discussionsParent}>
+                <div className={styles.discussionsList}>
+                    {data?.map((item)=>(
+                        <div className={styles.indivItem}>
+                            <img src={item.img} alt=""/>
+                            <div className={styles.itemNumber}>
+                                <p className={styles.itemNum}>{item.num}</p>
+                            </div>
+                            <div className={styles.itemTitle}>
+                                <h3>{item.title}</h3>
+                                <h5>{item.caption}</h5>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.discussionsRightBar}>
+                    <div className={styles.discussionsRightBar_1}>
+                        <button>CREATE NEW DISCUSSION</button>
+                        <p>
+                            Before joining or starting a discussion remember to always be civil. 
+                            Treat others with respect. Do not use the discussions board for 
+                            direct sales or self-promotion. 
+                            You are free to share your products and product ideas for feedback. 🙌
+                        </p>
+                    </div>
+                    <h3>Filters</h3>
+                    <div className={styles.discussionsRightBar_2}>
+                        <form>
+                            <input type="checkbox" name="amas"/>
+                            <label for="amas">AMAs</label>
+                            <br/>
+                            <input type="checkbox" name="amas"/>
+                            <label for="amas">Request for Feedback</label>
+                            <br/>
+                            <input type="checkbox" name="amas"/>
+                            <label for="amas">Help</label>
+                        </form>
+                    </div>
+                    <div className={styles.discussionsRightBar_3}>
+                        <h3>MOST HELPFUL THIS WEEK</h3>
+                        <img src='https://ph-avatars.imgix.net/2062078/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop' alt=''/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export {Discussions}
