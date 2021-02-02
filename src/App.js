c1f5d0b6f76bda251e44b18a3460befbf7d58127
Nitansh <nitansh11@gmail.com>
2021-02-02 11:47:07 +0530

    Basic Template 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
